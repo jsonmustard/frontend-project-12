e3dc5dc141cd4c663b023d8b9162bc75ff304bd0
Kirill Zheltov <kirillzheltov@gmail.com>
2024-12-26 12:34:26 -0300

    fix for render.com failed deploy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import store from './slices/index.js';
 import App from './App';
 import './styles.scss';
@@ -9,8 +10,19 @@ import 'bootstrap';
 import './i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const rollbarConfig = {
+  accessToken: 'bd0ba53e1b4f4c8d9feddbb300b30c80',
+  environment: 'testenv',
+};
+
+// eslint-disable-next-line functional/no-expression-statement
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <RollbarProvider config={rollbarConfig}>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ErrorBoundary>
+  </RollbarProvider>,
 );
